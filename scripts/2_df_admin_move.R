045e62bc6e30ca309e73cc124f0cfca2d64ab938
Gabrielle Crain <craingabs@gmail.com>
2025-06-04 11:44:57 -0400

    create new script and format header

---
+++ scripts/2_df_admin_move.R
@@ -0,0 +1,69 @@
+# ================================================================= ####
+# Notes to Script ####
+# -- Objective ####
+# Clean move data 
+# -- Readme ####
+# -- To do ####
+# ================================================================= ####
+# Set up ####
+# -- Prepare environment ####
+rm(list=ls())
+source("scripts/00_packages.R")
+source("scripts/0_utils.R")
+
+# -- Functions ####
+# Data cleaning tools 
+assess_variable <- function(x) {
+  # Check data type
+  data_type <- class(x)
+  
+  # Determine range if numeric
+  value_range <- if (is.numeric(x)|is.Date(x)) range(x, na.rm = TRUE) else NA
+  
+  # Count unique values
+  unique_values <- unique(x)
+  unique_count <- length(unique_values)
+  
+  # Get up to the first 100 unique values
+  unique_sample <- head(unique_values, 100)
+  
+  # Table the number of characters in a variable
+  nchar_table <- if(is.character(x)) table(nchar(x), useNA = "always") else NA
+  
+  # Count NA values
+  na_count <- sum(is.na(x))
+  
+  # Count NaN values
+  nan_count <- sum(is.nan(x))
+  
+  # Count empty values ("" for character variables)
+  empty_count <- if (is.character(x)) sum(grepl("^\\s*$", x), na.rm = TRUE) else 0
+  
+  # Return results as a list
+  result <- list(
+    data_type = data_type,
+    value_range = value_range,
+    unique_count = unique_count,
+    unique_sample = unique_sample,
+    nchar_table = nchar_table,
+    na_count = na_count,
+    nan_count = nan_count,
+    empty_count = empty_count
+  )
+  
+  return(result)
+}
+remove_leading_zeros <- function(x) {
+  # Remove leading zeros
+  cleaned_x <- sub("^0+", "", x)
+  
+  # If a value contained only zeros (now an empty string), replace with "0"
+  cleaned_x[grepl("^0*$", x)] <- "0"
+  
+  return(cleaned_x)
+}
+
+# -- Read in Data ####
+move <- readRDS("data/processed/processing_layer_2/move_masked.Rds")
+
+# ================================================================= ####
