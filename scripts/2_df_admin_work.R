987896d8404c472de43053124357e3c9c30cbafc
Gabrielle Crain <craingabs@gmail.com>
2025-06-09 11:30:49 -0400

    delete assess_variable

--- scripts/2_df_admin_work.R
+++ scripts/2_df_admin_work.R
@@ -20,45 +20,6 @@ source("scripts/00_packages.R")
 source("scripts/0_utils.R")
 
 # -- Functions ####
-assess_variable <- function(x) {
-  # Check data type
-  data_type <- class(x)
-  
-  # Determine range if numeric
-  value_range <- if (is.numeric(x)|is.Date(x)) range(x, na.rm = TRUE) else NA
-  
-  # Count unique values
-  unique_values <- unique(x)
-  unique_count <- length(unique_values)
-  
-  # Get up to the first 100 unique values
-  unique_sample <- head(unique_values, 100)
-  
-  # Table the number of characters in a variable
-  nchar_table <- if(is.character(x)) table(nchar(x), useNA = "always") else NA
-  
-  # Count NA values
-  na_count <- sum(is.na(x))
-  
-  # Count NaN values
-  nan_count <- sum(is.nan(x))
-  
-  # Count empty values ("" for character variables)
-  empty_count <- if (is.character(x)) sum(grepl("^\\s*$", x), na.rm = TRUE) else 0
-  
-  # Return results as a list
-  result <- list(
-    data_type = data_type,
-    value_range = value_range,
-    unique_count = unique_count,
-    unique_sample = unique_sample,
-    nchar_table = nchar_table,
-    na_count = na_count,
-    nan_count = nan_count,
-    empty_count = empty_count
-  )
-  
-  return(result)}
 
 standardize_job_field <- function(x) {
   # Step 1: Lowercase everything
