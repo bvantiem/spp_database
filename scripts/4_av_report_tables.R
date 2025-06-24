5edd70b717aa0b1827f7274e8b4ef80a23e69932
bvantiem <111447895+bvantiem@users.noreply.github.com>
2025-06-23 22:17:30 -0400

    finished treatment compliance table

--- scripts/4_av_report_tables.R
+++ scripts/4_av_report_tables.R
@@ -14,66 +14,7 @@ rm(list=ls())
 source("scripts/00_packages.R")
 source("scripts/0_utils.R")
 
-# -- Functions (delete if none) ####
-
-# Generate basic table with means ####
-# prepare_table <- function(data){
-#   # Note: numbering the rows because tidyr::gather puts variables in alphabetical order
-#   tab <- data %>%
-#     transmute(
-#       # Sentence Characteristics
-#       minimum_sentence = release_type_removed,
-#       maximum_sentence = sent_max_cort_days/365)
-#   # life = life)
-#   
-#   tab <- data.balance %>%
-#     transmute(
-#       # Sentence Characteristics
-#       r01a_minimum_sentence = sent_min_cort_days/365,
-#       r01b_maximum_sentence = sent_max_cort_days/365,
-#       # r01c_life = life,
-#       # Offense Characteristics
-#       r02a_violent_offense = violent_offense,
-#       r02b_property_offense = property_offense,
-#       r02c_drugs_offense = drugs_offense,
-#       r02d_publicorder_offense = publicorder_offense,
-#       # Demographics
-#       r03a_black = race_black,
-#       r03b_white = race_white,
-#       # r03c_age_at_treatment = age_at_treatment,
-#       r03d_married = married,
-#       r03e_high_school_degree = high_school)
-#   #r03e_security_threat_group = STG)
-#   
-#   tab <- tab %>%
-#     tidyr::gather(variable, value) %>%
-#     # Summarize by variable
-#     group_by(variable) %>%
-#     # summarise all columns
-#     summarise(`Mean` = round(mean(value, na.rm=TRUE),3)) %>% # Check number of NA values after getting full data.balance
-#     mutate(Mean = format(Mean, digits = 3))
-#   
-#   tab_2 <- data.balance %>%
-#     select(
-#       r99a_N = research_id) %>%
-#     tidyr::gather(variable, value) %>%
-#     # Summarize by variable
-#     group_by(variable) %>%
-#     # summarise all columns
-#     summarise(`Mean` = length(unique(value)))
-#   
-#   tab <- rbind(tab,
-#                tab_2)
-#   
-#   variable_names <- gsub("(....)(_)(.*)", "\\3", tab$variable)
-#   variable_names <- str_to_title(gsub("_", " ", variable_names))
-#   variable_names[which(variable_names=="Publicorder Offense")] <- "Public Order Offense"
-#   tab$variable <- variable_names
-#   names(tab) <- c("Variable", "Share/Mean")
-#   rownames(tab) <- NULL
-#   return(tab)
-# }
-
+# -- Functions ####
 
 # Read in data ####
 respart <- readRDS("data/processed/de_identified/3_research_participants_masked.Rds")
@@ -83,44 +24,45 @@ randassign <- readRDS("data/processed/de_identified/1b_randassign_masked.Rds") #
 # -- Prepare data ####
 treatment_compliance <- randassign %>%
   filter(rct == 1) %>%
-  filter(rct_stratum != "lifer") %>%
+  filter(rct_stratum %ni% c("lifer", "commuted death")) %>%
   mutate(onunit = ifelse(is.na(rct_release_dt), 1, 0)) 
 
 # Percents ####
-on_unit <- treatment_compliance %>% 
+tab_percents <- treatment_compliance %>% 
   group_by(rct_treat_wave) %>%
-  summarise(currently_on_the_lsu = mean(onunit)*100)
-
-released <- treatment_compliance %>% 
-  filter(onunit==0) %>%
-  group_by(rct_treat_wave) %>%
-  summarise(released = mean(rct_release_community)*100,
-            removed = mean(rct_release_removed)*100,
+  summarise(currently_on_unit = mean(onunit)*100,
+            released = mean(rct_release_community)*100,
             transferred = mean(rct_release_transferred)*100,
-            refused_treatment = mean(rct_release_refused)*100)
-
-tab_percents <- left_join(on_unit, released)
-tab_percents <- tab_percents %>%
+            removed = mean(rct_release_removed)*100,
+            refused_treatment = mean(rct_release_refused)*100) %>%
+  mutate(currently_on_unit = paste0(format(currently_on_unit, digits = 2), "%"),
+         released = paste0(format(released, digits = 2), "%"),
+         removed = paste0(format(removed, digits = 2), "%"),
+         transferred = paste0(format(transferred, digits = 2), "%"),
+         refused_treatment = paste0(format(refused_treatment, digits = 2), "%")) %>%
   mutate(row_type = "percent")
-tab_percents
+
 
 # Numbers ####
 on_unit_numbers <- treatment_compliance %>% 
   group_by(rct_treat_wave) %>%
-  summarise(currently_on_the_lsu = sum(onunit))
+  summarise(currently_on_unit = sum(onunit)) %>%
+  mutate(currently_on_unit = as.character(round(currently_on_unit)))
 
-released_numbers <- treatment_compliance %>% 
-  filter(onunit==0) %>%
+tab_numbers <- treatment_compliance %>% 
   group_by(rct_treat_wave) %>%
-  summarise(released = sum(rct_release_community),
-            removed = sum(rct_release_removed),
+  summarise(currently_on_unit = sum(onunit),
+            released = sum(rct_release_community),
             transferred = sum(rct_release_transferred),
-            refused_treatment = sum(rct_release_refused))
-
-tab_numbers <- left_join(on_unit_numbers, released_numbers)
-tab_numbers <- tab_numbers %>%
+            removed = sum(rct_release_removed),
+            refused_treatment = sum(rct_release_refused)) %>%
+  mutate(currently_on_unit = as.character(round(currently_on_unit)),
+         released = as.character(round(released)),
+         removed = as.character(round(removed)),
+         transferred = as.character(round(transferred)),
+         refused_treatment = as.character(round(refused_treatment))) %>%
   mutate(row_type = "numbers")
-tab_numbers
+
 
 # Combine dataframes
 tab_combined <- bind_rows(tab_numbers, tab_percents) |>
@@ -132,27 +74,46 @@ tab_combined <- bind_rows(tab_numbers, tab_percents) |>
 totals <- tab_numbers |>
   summarise(
     treatment_wave = "Total",
-    currently_on_the_lsu = sum(currently_on_the_lsu, na.rm = TRUE),
-    released = sum(released, na.rm = TRUE),
-    removed = sum(removed, na.rm = TRUE),
-    transferred = sum(transferred, na.rm = TRUE),
-    refused_treatment = sum(refused_treatment, na.rm = TRUE)
+    currently_on_unit = sum(as.numeric(currently_on_unit, na.rm = TRUE)),
+    released = sum(as.numeric(released), na.rm = TRUE),
+    removed = sum(as.numeric(removed), na.rm = TRUE),
+    transferred = sum(as.numeric(transferred), na.rm = TRUE),
+    refused_treatment = sum(as.numeric(refused_treatment), na.rm = TRUE)
   )
 
 tab_combined_with_totals <- rbind(tab_combined, totals)
+names(tab_combined_with_totals) <- str_to_title(gsub("_", " ", names(tab_combined_with_totals)))
 
-# Latex table #### o
-tab_latex <- tab_combined_with_totals %>%
+# PDF table #### 
+tab_pdf<- tab_combined_with_totals %>%
   kbl(caption = "Treatment Compliance",
       align = c("lrrrrr"),
       row.names = FALSE) %>%
+  add_header_above(c(" " = 2, "Not on Unit" = 4)) %>%
   kable_classic(full_width = F,
                 html_font = "Times New Roman") %>%
   row_spec(c(14,15),
            hline_after=TRUE,
-           extra_css = "border-bottom: 1px solid") 
+           extra_css = "border-bottom: 1px solid")
+  
 
-# -- Save ####
-# tab_latex <- save_kable(file = "output/tables/tabx_balance_table_waves123456.pdf", self_contained = T,density = 200)
+tab_pdf
+
+# Latex Table ####
+tab_combined_with_totals %>%
+  kbl(caption = "Treatment Compliance",
+      align = c("lrrrrr"),
+      row.names = FALSE,
+      format = "latex", 
+      booktabs = TRUE) %>%
+  add_header_above(c(" " = 2, "Not on Unit" = 4)) %>%
+  kable_classic(full_width = F,
+                html_font = "Times New Roman") %>%
+  row_spec(c(14,15),
+           hline_after=TRUE,
+           extra_css = "border-bottom: 1px solid") %>%
+  save_kable(file = "output/tables/tabx_treatment_compliance.tex", self_contained = T)
+
+
 
 
