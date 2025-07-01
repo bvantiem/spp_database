3bed0052e7b0b903abc4011f803ef0709f2c545a
Gabrielle Crain <craingabs@gmail.com>
2025-07-01 11:18:58 -0400

    do assess_variable, add notes on some findings

--- scripts/temp_conduct_av_report.R
+++ scripts/temp_conduct_av_report.R
@@ -48,19 +48,67 @@ names(conduct_2)[which(names(conduct_2) %ni% names(conduct_raw))]
 # Some places to start digging.. ####
 # 1. Assess the variables we've got in conduct_2 ####
 assess_variable(conduct_2$control_number)
+# -- 548 unique inmate_numbers/ inmate_id_nums
+assess_variable(conduct_2$state_id_num)
+assess_variable(conduct_2$inmate_number)
+assess_variable(conduct_2$LSStartDate)
+# -- refers to the date they started on LS 
+# -- -- missing wave 7!
+assess_variable(conduct_2$RandomAssignment)
+assess_variable(conduct_2$misconduct_number)
+assess_variable(conduct_2$institution)
+# -- more than just CHS
+assess_variable(conduct_2$misconduct_date)
+assess_variable(conduct_2$MisconductDate)
+# -- only includes post-treat misconducts 
+assess_variable(conduct_2$category_charge1)
+assess_variable(conduct_2$chrg_description)
+assess_variable(conduct_2$place_hvl_code)
+# -- location of misconduct
+assess_variable(conduct_2$place_hvl_desc)
+assess_variable(conduct_2$inmte_plea_gilty)
+assess_variable(conduct_2$inmte_plea_ngilty)
+assess_variable(conduct_2$inmte_plea_nplea)
+assess_variable(conduct_2$vrdict_guilty)
+assess_variable(conduct_2$vrdict_nguilty)
+assess_variable(conduct_2$vrdict_dismis_wp)
+assess_variable(conduct_2$vrdict_dismis_wop)
+assess_variable(conduct_2$vrdict_reduced)
+assess_variable(conduct_2$data_followup)
+assess_variable(conduct_2$research_id)
 
 # 2. Are all research_ids in conduct_2 also in conduct and vice versa? ####
 ids_conduct <- unique(conduct$research_id)
 ids_conduct_2 <- unique(conduct_2$research_id)
-ids_conduct[which(ids_conduct %ni% ids_conduct_2)]
-ids_conduct_2[which(ids_conduct_2 %ni% ids_conduct)]
+# In conduct but not in conduct_2
+missing_from_conduct_2 <- conduct %>%
+  filter(!research_id %in% conduct_2$research_id)
+
+# In conduct_2 but not in conduct
+missing_from_conduct <- conduct_2 %>%
+  filter(!research_id %in% conduct$research_id)
+# -- reserach_id's missing from conduct
+# rid_061684
+# rid_067282
+# rid_039819
+# rid_845680
+# rid_514153
+# rid_015455
+# rid_033410
+# rid_016680
+# rid_025983
+# rid_069834
+# rid_033761
 
 # 3. Is there a pattern to which research_ids are missing? ####
 # -- just treated/control?
 # -- just chester?
+# -- -- other facilties included
 # -- just a certain type of charge/guilty finding?
 # -- just certain waves? (wave 7 missing?)
+# -- -- wave 7 missing
 # -- date ranges?
+# -- -- 2022-06-07 -> 2025-05-16 (conduct df is 2000-07-06 -> 2025-06-20)
 # -- other ideas?
 # -- Ideas on how to check this
 # -- -- Use assess_variable
